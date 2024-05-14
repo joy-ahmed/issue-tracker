e88387bb64f10ec6f5bc71b73eecea44bfd58569
Joy Ahmed <dev.joyahmed@gmail.com>
2024-05-15 01:08:47 +0600

    added shadcn

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import React from "react";
 
 const IssuesPage = () => {
   return (
-    <div>IssuesPage</div>
-  )
-}
+    <div>
+      <h1>Issues</h1>
+      <Button className="bg-emerald-500 hover:bg-emerald-600">
+        <Plus className="w-4 h-4 mr-2" />
+        New Issue
+      </Button>
+    </div>
+  );
+};
 
-export default IssuesPage
+export default IssuesPage;
