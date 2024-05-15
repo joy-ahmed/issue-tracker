3da05b749f57ff495bce63b3211f18503300489b
Joy Ahmed <dev.joyahmed@gmail.com>
2024-05-15 19:45:31 +0600

    added aditonal loading

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import { Skeleton } from '@/components/ui/skeleton'
+
 const LoadingNewPage = () => {
   return (
-    <div>Loading...</div>
+    <div className="container mx-auto">
+      <div className="border p-4 rounded">
+        <div>
+          <Skeleton className="h-5 w-32" />
+          <div className="flex items-center space-x-2 my-2">
+            <Skeleton className="h-5 w-32" />
+            <Skeleton className="h-5 w-32" />
+          </div>
+        </div>
+        <hr className="py-2" />
+        <div className="prose">
+          <Skeleton className="h-5 w-32 mb-4" />
+          <Skeleton className="h-5 w-32 mb-4" />
+          <Skeleton className="h-5 w-32" />
+        </div>
+      </div>
+    </div>
   )
 }
 
