f0c3b1fadeb175c0f164ccbb5773a34993b9f93b
Joy Ahmed <dev.joyahmed@gmail.com>
2024-05-15 21:08:19 +0600

    added update issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,6 @@
+import { schema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
+import { notFound } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest, { params }: { params: { id: string }}) {
@@ -8,4 +10,27 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
         }
     })
     return NextResponse.json(issue)
+}
+
+export async function PUT(request: NextRequest, { params }: { params: { id: string }}) {
+    const issue = await prisma.issue.findUnique({
+        where: {
+            id: params.id
+        }
+    })
+    if (!issue) notFound();
+    const body = await request.json();
+    const validated = schema.safeParse(body);
+    if(!validated.success) return NextResponse.json(validated.error.errors, { status: 400 })
+    const { title, description } = validated.data;
+    const updatedIssue = await prisma.issue.update({
+        where: {
+            id: params.id
+        },
+        data: {
+            title,
+            description
+        }
+    })
+    return NextResponse.json(updatedIssue, { status: 200 })
 }
