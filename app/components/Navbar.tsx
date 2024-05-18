d26d8a345aaa671ebbddfb95ee0f10b80d63e1a5
Joy Ahmed <dev.joyahmed@gmail.com>
2024-05-18 15:29:37 +0600

    showing user details on nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,18 +2,32 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
-import { TiFlowSwitch } from "react-icons/ti";
+import { FaFire } from "react-icons/fa6";
+import { useSession } from 'next-auth/react'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { IoIosPower } from "react-icons/io";
 
 const Navbar = () => {
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+  const { data: session, status } = useSession()
   const pathname = usePathname();
   return (
-    <nav className="flex space-x-6 border-b px-5 h-16 items-center mb-5">
+    <nav className="flex border-b px-16 h-16 items-center justify-between mb-5">
+      <div className="flex items-center space-x-8">
       <Link href="/">
-        <TiFlowSwitch size={36} className="text-emerald-500" />
+      <FaFire size={36} fill="#10B981" />
       </Link>
       <ul className="flex gap-3">
         {links.map((link) => (
@@ -29,6 +43,38 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      </div>
+      <div className="">
+        { status === 'authenticated' && (
+        // <Link href="/api/auth/signout">Sign out</Link>
+        <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div>
+          <Avatar>
+            <AvatarImage src={session?.user?.image!} />
+            <AvatarFallback>{session?.user?.name?.charAt(0)}</AvatarFallback>
+          </Avatar>
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="mr-2">
+        <DropdownMenuLabel>{session?.user?.email}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link href="/api/auth/signout" className="flex items-center gap-2">
+            <IoIosPower size={20} />
+            Sign out
+            </Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+        )}
+        { status === 'unauthenticated' && <Link href="/api/auth/signin">Sign in</Link> }
+      </div>
     </nav>
   );
 };
